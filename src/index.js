a4ba2f9f99cb2f4ff3182c2e487c53ead1c3e91f
Yevhen Fabizhevskyi <fabasoad@gmail.com>
2020-03-18 23:08:49 +0900

    Fix issues

--- src/index.js
+++ src/index.js
@@ -8,19 +8,15 @@ const fs = require('fs');
 
 const VOICE_FILE = 'src/voice.generated.xml';
 
-async function run() {
-  const oldContent = fs.readFileSync('src/voice.xml', { encoding: 'utf8' });
-  process.stdout.write(oldContent);
-  const newContent = oldContent.replace('${text}', core.getInput('text'));
-  await fs.writeFile(VOICE_FILE, newContent);
+const oldContent = fs.readFileSync('src/voice.xml', 'utf8');
+process.stdout.write(oldContent);
+const newContent = oldContent.replace('${text}', core.getInput('text'));
+fs.writeFileSync(VOICE_FILE, newContent);
 
-  await client.calls
-    .create({
-      from: core.getInput('from'),
-      to: core.getInput('to'),
-      url: VOICE_FILE
-    })
-    .then(call => process.stdout.write(call));
-}
-
-run();
+client.calls
+  .create({
+    from: core.getInput('from'),
+    to: core.getInput('to'),
+    url: VOICE_FILE
+  })
+  .then(call => process.stdout.write(call));
