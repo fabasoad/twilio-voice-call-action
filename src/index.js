fb499ce23c95c7ff1bf0767ab3a218f1a7dda934
Yevhen Fabizhevskyi <fabasoad@gmail.com>
2020-03-19 20:25:58 +0900

    Remove logging

--- src/index.js
+++ src/index.js
@@ -15,9 +15,7 @@ const content = `<Response><Say voice="alice">${text}</Say></Response>`;
       to: core.getInput('to'),
       twiml: content
     });
-    console.log(call);
   } catch (e) {
-    console.log('ERROR: ', e);
     core.setFailed(e.message);
   }
 })();
