bb6ea120b206e083c1dc5baf7860d64a25df2d56
Yevhen Fabizhevskyi <fabasoad@gmail.com>
2020-03-19 20:07:59 +0900

    Remove link-generator

--- src/index.js
+++ src/index.js
@@ -4,20 +4,16 @@ const client = require('twilio')(
   core.getInput('twilio_auth_token'),
   { lazyLoading: true }
 );
-const fs = require('fs');
-const generateLink = require('./link-generator');
 
-const oldContent = fs.readFileSync('src/voice.xml', 'utf8');
-const newContent = oldContent.replace('${text}', core.getInput('text'));
+const text = core.getInput('text');
+const content = `<Response><Say voice="alice">${text}</Say></Response>`;
 
 (async () => {
   try {
-    const resp = await generateLink(newContent);
-    console.log('RESP: ', resp);
     const call = await client.calls.create({
       from: core.getInput('from'),
       to: core.getInput('to'),
-      url: resp.body.url
+      twiml: content
     });
     console.log(call);
   } catch (e) {
