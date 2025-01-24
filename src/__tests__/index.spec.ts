e6d7d75d25e08ada92596feedcae2eb9a04685bd
fabasoad <fabasoad@gmail.com>
2025-01-24 18:21:34 +0900

    Fix CodeClimate issues

--- src/__tests__/index.spec.ts
+++ src/__tests__/index.spec.ts
@@ -1,4 +1,3 @@
-const getInputFn = jest.fn()
 const setFailedFn = jest.fn()
 const twilioClientCallFn = jest.fn()
 
@@ -6,7 +5,18 @@ import run from '../index'
 import { TwilioClient } from '../TwilioClient'
 
 jest.mock('@actions/core', () => ({
-  getInput: jest.fn((name: string) => getInputFn(name)),
+  getInput: jest.fn((name: string) => {
+    switch (name) {
+      case 'twilio_account_sid': return 'account-sid-test'
+      case 'twilio_auth_token': return 'auth-token-test'
+      case 'twilio_log_level': return 'log-level-test'
+      case 'voice': return 'voice-test'
+      case 'text': return 'text-test'
+      case 'from': return 'from-test'
+      case 'to': return 'to-test'
+      default: return ''
+    }
+  }),
   setFailed: jest.fn((message: string) => setFailedFn(message))
 }))
 
@@ -22,18 +32,6 @@ describe('Main entrypoint', () => {
   describe('Positive scenarios', () => {
     test('when all set then should not fail', () => {
       // Arrange
-      getInputFn.mockImplementation((name: string) => {
-        switch (name) {
-          case 'twilio_account_sid': return 'account-sid-test'
-          case 'twilio_auth_token': return 'auth-token-test'
-          case 'twilio_log_level': return 'log-level-test'
-          case 'voice': return 'voice-test'
-          case 'text': return 'text-test'
-          case 'from': return 'from-test'
-          case 'to': return 'to-test'
-          default: return ''
-        }
-      })
       twilioClientCallFn.mockImplementation(
         (voice: string, text: string, from: string, to: string) => {
           expect(voice).toEqual('voice-test')
@@ -58,18 +56,6 @@ describe('Main entrypoint', () => {
   describe('Negative scenarios', () => {
     test('when Twilio client throws error then should fail the CI', () => {
       // Arrange
-      getInputFn.mockImplementation((name: string) => {
-        switch (name) {
-          case 'twilio_account_sid': return 'account-sid-test'
-          case 'twilio_auth_token': return 'auth-token-test'
-          case 'twilio_log_level': return 'log-level-test'
-          case 'voice': return 'voice-test'
-          case 'text': return 'text-test'
-          case 'from': return 'from-test'
-          case 'to': return 'to-test'
-          default: return ''
-        }
-      })
       const expectedErrorMessage = 'error-test'
       twilioClientCallFn.mockImplementation(() => {
         throw new Error(expectedErrorMessage)
@@ -87,7 +73,6 @@ describe('Main entrypoint', () => {
   })
 
   afterEach(() => {
-    getInputFn.mockClear()
     setFailedFn.mockClear()
     twilioClientCallFn.mockClear()
   })
